a6f33526e9324f498864ce7822335a4c6ce161c2
Medtech-code <prarthanaduraisamy2605@gmail.com>
2025-12-08 17:05:50 +0100

    run_inference changed

--- Python/run_inference.py
+++ Python/run_inference.py
@@ -4,11 +4,13 @@ import config
 from src.data_loader import load_holdout_data
 from src.preprocessing import preprocess
 from src.feature_extraction import extract_features
+from src.feature_selection import select_features
 from src.inference import make_inference, generate_submission_file
 from src.utils import save_cache, load_cache
 import os
 import joblib
 import glob
+
 
 def process_holdout_file(file_path, model, config):
     """
@@ -49,29 +51,56 @@ def process_holdout_file(file_path, model, config):
         if config.USE_CACHE:
             save_cache(preprocessed_holdout_data, cache_filename_preprocess_holdout, config.CACHE_DIR)
             
+
     # 3. Feature Extraction (using the same logic as training)
     holdout_features = None
     cache_filename_features_holdout = f"features_holdout_iter{config.CURRENT_ITERATION}.joblib"
     if config.USE_CACHE:
         holdout_features = load_cache(cache_filename_features_holdout, config.CACHE_DIR)
-
+        print(holdout_features)
     if holdout_features is None:
         try:
-            holdout_features = extract_features(preprocessed_holdout_data, config, channel_info)
+            if config.CURRENT_ITERATION==2:
+                preprocessed_holdout_data_eeg=preprocessed_holdout_data['eeg'][:, 0, :]
+                preprocessed_holdout_data_eog=preprocessed_holdout_data['eog'][:,0,:]
+                holdout_features,feature_names = extract_features([preprocessed_holdout_data_eeg,preprocessed_holdout_data_eog], config, channel_info)
+
+            else:
+
+                holdout_features,feature_names = extract_features(preprocessed_holdout_data, config, channel_info)
+    
+
         except Exception as e:
             print(f"error! Failed feature extraction for {record_id}: {e}")
             return None        
         if config.USE_CACHE:
             save_cache(holdout_features, cache_filename_features_holdout, config.CACHE_DIR)
 
+    # Feature selection
+    selected_holdout_features=None
+    cache_filename_selectedfeatures_holdout = f"features_selectedholdout_iter{config.CURRENT_ITERATION}.joblib"
+    if config.USE_CACHE:
+        holdout_selected_features = load_cache(cache_filename_selectedfeatures_holdout, config.CACHE_DIR)
+
+    if selected_holdout_features is None:
+        try:
+            selected_indices = np.load(f"cache\selected_indices_iter{config.CURRENT_ITERATION}.npy")
+            holdout_selected_features = holdout_features[:, selected_indices]
+        except Exception as e:
+            print(f"error! Failed feature extraction for {record_id}: {e}")
+            return None        
+        if config.USE_CACHE:
+            save_cache(holdout_selected_features, cache_filename_selectedfeatures_holdout, config.CACHE_DIR)
+
+    
     # 4. Make Inference
     try:
-        prediction = make_inference(model, holdout_features, config)
+        prediction = make_inference(model, holdout_selected_features, config)
         print(f"Prediction for {record_id} successful")
         return prediction, record_info
     except Exception as e:
         print(f"error! Failed prediction for {record_id}: {e}")
-        return
+        return None
         
         
 def run_inference():
